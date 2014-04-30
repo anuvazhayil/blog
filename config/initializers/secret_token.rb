35c477f0a439bcf7eb6efb1cbbb95c4064fba5b6
root <root@cpt-anu.(none)>
2014-04-30 21:07:17 +0530

    Authenticate to create your blog

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '28d56421ce365ca5c79a49a07a33f963e369191baa76391e0064f43f22571be68352c8e4d4be1b3c10ec1d93f9f62c91836022bbc5f1ae8562564edcade1f355'
